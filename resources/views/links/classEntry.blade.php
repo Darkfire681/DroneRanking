13b0f52aa18316fcc686c5270997b51a3b59a1b0
Darkfire681 <andre.rojas.esquivel@gmail.com>
2019-06-14 10:53:53 -0600

    Adding classentry page form for testing

--- resources/views/links/classEntry.blade.php
+++ resources/views/links/classEntry.blade.php
@@ -22,10 +22,28 @@
   <table class="table table-hover table-sm">
     <tr>
       <th width = "50px">bug<b>No.</b></th>
-      <th width = "300px">Test Name Andre</th>
-      <th>Testing, Not Sure</th>
+      <th width = "300px">classId</th>
+      <th>className</th>
       <th width = "180px">Action</th>
     </tr>
 
+    @foreach ($classentrys as $classentry)
+      <tr>
+        <td><b>{{++$i}}.<b/></td>
+        <td>{{$classentry->classId}}</td>
+        <td>{{$classentry->className}}</td>
+        <td>
+          <form class="" action="{{ route('classentry.destroy', $classentry->id) }}" method="post">
+            <a class="btn btn-sm btn-success" href="{{ route('classentry.show', $classentry->id) }}">Show</a>
+            <a class="btn btn-sm btn-warning" href="{{ route('classentry.edit', $classentry->id) }}">Edit</a>
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="btn btn-sm btn-danger">Delete</button>
+          </form>
+        </td>
+      </tr>
+    @endforeach
   </table>
+  {!! $classentrys->links() !!}
 </div>
+@endsection
